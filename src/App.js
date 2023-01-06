fe35e1525de0f6342307d11bb1cef8a4c28cf8ca
Muhammad Mubashir Munir Khan <100374421+mubashir05-beep@users.noreply.github.com>
2023-01-06 03:39:47 -0800

    installed tailwind

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from "react";
-import "./style.css";
+import './app.css';
 import {Navbar} from './components/export';
 export default function App() {
   return (
--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import logo from '../../assets/logo.svg';
 const ListItems = (props) => {
   return (
-    <li className="text-white ">
+    <li className="text-white text-lg hover:text-blue ">
       <a href={props.name}>{props.menu}</a>
     </li>
   );
@@ -15,15 +15,15 @@ const Navbar = () => {
     { id: 4, menu: 'Client', name: '#client' },
   ];
   return (
-    <div className=" bg-black px-6 py-3 flex flex-row items-center  justify-between">
-      <img className="cursor-pointer h-8" src={logo} alt="Hoobank Logo" />
+    <div className=" bg-black px-8 py-6 flex flex-row items-center  justify-between">
+      <img className="cursor-pointer h-12" src={logo} alt="Hoobank Logo" />
       <div className="flex gap-6">
-        <ul className="flex flex-row justify-evenly items-center gap-4">
+        <ul className=" flex flex-row justify-evenly items-center gap-4 mx-12">
           {menu_Items.map((item) => (
             <ListItems key={item.id} menu={item.menu} name={item.name} />
           ))}
         </ul>
-        <button className="bg-teal-400 font-semibold rounded-lg px-3 py-2 border-solid hover:bg-teal-600 hover:ease-in-out duration-300">
+        <button className="bg-teal-400 font-medium rounded-lg px-3 py-2 border-solid hover:bg-teal-600 hover:ease-in-out duration-300">
           Get Started
         </button>
       </div>
