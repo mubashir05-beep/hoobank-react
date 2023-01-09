ad966c1cc8ecf40ba43b9734e5df07752cfc6c55
Muhammad Mubashir Munir Khan <100374421+mubashir05-beep@users.noreply.github.com>
2023-01-09 06:41:59 -0800

    adding styles

--- src/components/header/Head.jsx
+++ src/components/header/Head.jsx
@@ -4,18 +4,23 @@ import './head.css'
 import robot from '../../assets/robot.png';
 const Head = () => {
   return (
-    <div className="flex flex-col ">
+    <div className="flex flex-col mt-10">
       <div className="m-4">
         <div className="flex flex-row text-slate items-center gap-1">
           <img src={discount} alt="DiscountLogo" />
           <span className="font-semibold text-white">20%</span>Discount for{" "}
           <span className="text font-semibold ">1 MONTH</span> Account
         </div>
-        <h1>
+        <h1 className="text-white font-bold text-6xl">
           The Next
-          <br /> Generation <br />
+          <br />
+          <span className="text font-light">Generation</span> <br />
           Payment Method.
         </h1>
+        <p className="text-slate text-lg w-60">
+          Our team of experts uses a methodology to identify the credit cards
+          most likely to fit your needs.
+        </p>
       </div>
       <div className="">
         <img src={robot} alt="" srcset="" />
